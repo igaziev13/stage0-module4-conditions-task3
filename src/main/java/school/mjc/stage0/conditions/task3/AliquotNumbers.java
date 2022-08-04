0b2bd547dd1cc3824f840936780733469b4334bd
IGAZIEV <islam.gaziev@gmail.com>
2022-08-04 20:42:10 +0500

    Module #4.1. Task #3. Attempt #1.

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        float aliquot = first % second;
+        if(aliquot == 0)
+            System.out.println("Aliquot");
+        else
+            System.out.println("Not aliquot");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        char c = Character.toLowerCase(character);
+        boolean vowel = c == 'a' || c == 'o' || c == 'e' || c == 'i'|| c == 'u';
+        if (vowel)
+            System.out.println("Vowel");
+        else if (c >= 'b' && c <= 'z')
+            System.out.println("Consonant");
+        else
+            System.out.println("wrong alphabet!");
     }
 }
