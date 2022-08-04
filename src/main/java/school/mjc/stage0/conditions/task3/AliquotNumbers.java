3d4dcaad2bc310b964d1a7d256c8f1bec1503e49
IGAZIEV <islam.gaziev@gmail.com>
2022-08-04 22:18:57 +0500

    Module #4.1. Task #3. Attempt #2.

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
-        float aliquot = first % second;
+        float aliquot = second % first;
         if(aliquot == 0)
             System.out.println("Aliquot");
         else
